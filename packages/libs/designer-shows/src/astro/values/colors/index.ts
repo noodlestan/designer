678057123bdfd8362cef9f52d4947de530425e22
vanessa <vanessapcbarros@gmail.com>
2025-01-05 17:04:52 +0100

    add SRGB to components name

--- packages/libs/designer-shows/src/astro/values/colors/index.ts
+++ packages/libs/designer-shows/src/astro/values/colors/index.ts
@@ -1,5 +1,5 @@
 // @index(['./*.astro','./!(private|functions)*/index.ts}'], f => `export { default as ${f.name.replace(/\/index$/, '')} } from '${f.path}.astro';`)
 export { default as ShowColorValue } from './ShowColorValue.astro';
-export { default as ShowHueValue } from './ShowHueValue.astro';
-export { default as ShowLightnessValue } from './ShowLightnessValue.astro';
-export { default as ShowSaturationValue } from './ShowSaturationValue.astro';
+export { default as ShowSRGBHueValue } from './ShowSRGBHueValue.astro';
+export { default as ShowSRGBLightnessValue } from './ShowSRGBLightnessValue.astro';
+export { default as ShowSRGBSaturationValue } from './ShowSRGBSaturationValue.astro';
