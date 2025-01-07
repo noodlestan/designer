77888818a5dfcd52e195ac5eae3bb67d9c6f15b5
Andre Torgal <hello@andretorgal.com>
2025-01-07 15:42:51 +0100

    move sample data to designer-decisions package

---
+++ packages/libs/designer-decisions/src/sample-data/constants.ts
@@ -0,0 +1,12 @@
+import type { DecisionSource } from '../types';
+
+const SAMPLE_DATA: DecisionSource = {
+    name: 'Designer Decisions Sample Data',
+    source: {
+        type: 'package',
+        package: '@noodlestan/designer-decisions',
+        path: 'sample-data/',
+    },
+};
+
+export { SAMPLE_DATA };
