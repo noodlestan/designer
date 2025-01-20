4a7cc6d24a69ac98a238cc66939d2b23698441e5
Andre Torgal <hello@andretorgal.com>
2025-01-20 15:44:45 +0100

    add GridList layout component (WIP)

--- packages/libs/designer-shows/src/astro/layouts/index.ts
+++ packages/libs/designer-shows/src/astro/layouts/index.ts
@@ -1,3 +1,4 @@
 // @index(['./!(private|functions)*/index.ts'], f => `export * from '${f.path.replace(/\/index$/, '')}';`)
 export * from './cards';
 export * from './flex';
+export * from './grid';
