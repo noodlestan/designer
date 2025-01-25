539f82c24d8280315ba210084afe6bc604d79738
Andre Torgal <hello@andretorgal.com>
2025-01-26 00:38:30 +0100

    simplify some tests

--- packages/libs/designer-decisions/src/decisions/errors/createInputNotFoundError.test.ts
+++ packages/libs/designer-decisions/src/decisions/errors/createInputNotFoundError.test.ts
@@ -1,22 +1,18 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
-import type { DecisionError, DecisionRef } from '../../types';
+import type { DecisionRef } from '../../types';
 import { createDecisionContext } from '../createDecisionContext';
 
 import { createInputNotFoundError } from './createInputNotFoundError';
 
 describe('createInputNotFoundError()', () => {
-    const ref: DecisionRef = { $uuid: 'test-uuid' };
-    const context = createDecisionContext(ref, vi.fn(), []);
-
-    let result: DecisionError;
-
     describe('Given a context and a ref', () => {
-        beforeEach(() => {
-            result = createInputNotFoundError(context, ref);
-        });
+        const ref: DecisionRef = { $uuid: 'test-uuid' };
+        const context = createDecisionContext(ref, vi.fn(), []);
 
         it('should return a DecisionError object with the expected message', () => {
+            const result = createInputNotFoundError(context, ref);
+
             const expectedMessage = `Ref ${JSON.stringify(ref)} not found.`;
             expect(result.msg).toBe(expectedMessage);
         });
--- packages/libs/designer-decisions/src/decisions/errors/createUnexpectedError.test.ts
+++ packages/libs/designer-decisions/src/decisions/errors/createUnexpectedError.test.ts
@@ -1,6 +1,5 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
-import type { DecisionError } from '../../types';
 import { createDecisionContext } from '../createDecisionContext';
 
 import { createUnexpectedError } from './createUnexpectedError';
@@ -9,27 +8,21 @@ describe('createUnexpectedError()', () => {
     const ref = { $uuid: 'test-uuid' };
     const context = createDecisionContext(ref, vi.fn(), []);
 
-    let result: DecisionError;
-
     describe('Given a context and an error', () => {
         const mockError = new Error('Test error');
 
-        beforeEach(() => {
-            result = createUnexpectedError(context, mockError);
-        });
-
         it('should return a DecisionError object with the expected message', () => {
+            const result = createUnexpectedError(context, mockError);
+
             const expectedMessage = `Unexpected error in {"$uuid":"test-uuid"}: ${mockError.stack}.`;
             expect(result.msg).toBe(expectedMessage);
         });
     });
 
     describe('Given a context and no error', () => {
-        beforeEach(() => {
-            result = createUnexpectedError(context);
-        });
-
         it('should return a DecisionError object with the expected message', () => {
+            const result = createUnexpectedError(context);
+
             const expectedMessage = `Unexpected error in {"$uuid":"test-uuid"}: undefined.`;
             expect(result.msg).toBe(expectedMessage);
         });
