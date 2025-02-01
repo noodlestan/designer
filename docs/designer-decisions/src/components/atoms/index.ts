03a41f3289b210758748553fd77c9eb8cafc2830
Andre Torgal <hello@andretorgal.com>
2025-02-01 19:00:03 +0100

    Add banners to "Welcome" guide and API reference index (#125)

--- docs/designer-decisions/src/components/atoms/index.ts
+++ docs/designer-decisions/src/components/atoms/index.ts
@@ -1,2 +1,3 @@
 // @index(['./*.astro','./!(private|functions)*/index.ts}'], f => `export { default as ${f.name.replace(/\/index$/, '')} } from '${f.path}.astro';`)
 export { default as GetInvolved } from './GetInvolved.astro';
+export { default as LibraryStatus } from './LibraryStatus.astro';
