6d8717b84491a8613ec589e950d4616546fdd265
Vanessa <vanessapcbarros@gmail.com>
2025-02-01 18:43:56 +0100

    Create oklch models and components (#126)

---
+++ packages/libs/designer-shows/src/astro/decisions/color/color/index.ts
@@ -0,0 +1,3 @@
+// @index(['./*.astro','./!(private|functions)*/index.ts}'], f => `export { default as ${f.name.replace(/\/index$/, '')} } from '${f.path}.astro';`)
+export { default as ShowColorSetDecision } from './ShowColorSetDecision.astro';
+export { default as ShowColorValueDecision } from './ShowColorValueDecision.astro';
